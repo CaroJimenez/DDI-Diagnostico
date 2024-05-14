36057b542222367206e5ac22edf9649e4d55ca8e
CaroJimenez <20213tn053@utez.edu.mx>
2024-05-13 18:15:06 -0600

    sign lenguaje

--- app.js
+++ app.js
@@ -12,9 +12,27 @@ app.get("/api", (req, res) => {
 app.post("/validate", function (req, res) {
   const word = req.body.word;
   const regex = /^[a-zA-Z\s]+$/;
+  const split_string = word.split("");
+  const lowerCaseArray = split_string.map((item) => item.toLowerCase());
+
+  const code = [];
+
+  for (var i = 0; i < split_string.length; i++) {
+    const char = lowerCaseArray[i];
+    if (char === " " || char === "") {
+      code.push([" ", "no url"]);
+    } else {
+      code.push([
+        char,
+        `https://img.icons8.com/ios/50/sign-language-${split_string[
+          i
+        ].toLowerCase()}.png`,
+      ]);
+    }
+  }
 
   if (regex.test(word)) {
-    res.json({ message: "Word is valid" });
+    res.json({ message: "Word is valid", code });
   } else {
     res.status(400).json({ error: "Word contains symbols" });
   }
